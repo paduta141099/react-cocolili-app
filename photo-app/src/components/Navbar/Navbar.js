dee43e3f48a2c451f807ca95f233a6cf25ecca65
paduta141099 <54402512+paduta141099@users.noreply.github.com>
2022-04-05 18:48:40 +0700

    first-commit

---
+++ photo-app/src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-light bg-light fw-4 shadow-sm">
+            <div className="container-fluid">
+                <span class="navbar-brand mb-0 h1">Cocolili</span>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
